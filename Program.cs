18df4dcf8f9dc02b0e54fa495099a3365c0451e8
Hmorgan22 <huntermorgan1219@yahoo.com>
2023-10-30 14:45:29 -0400

    Test commit to group repo

--- Program.cs
+++ Program.cs
@@ -17,6 +17,9 @@ namespace CSC440_GroupProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new HomePageForm());
+               
+
+
         }
     }
 }
